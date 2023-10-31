a68e3ed0daca83890b1e6a5b060a975f431ba695
Dário <147437499+dariobrito01@users.noreply.github.com>
2023-10-31 22:37:36 +0300

    Add files via upload

---
+++ darioatividade23.py
@@ -0,0 +1,6 @@
+# Exercício Python 23: Faça um programa que calcule a soma entre todos os números que são múltiplos de três
+# e que se encontram no intervalo de 1 até 500.
+
+for n in range(1, 500, 3):
+    print(n)
+    n = int(input("informe um numero entre 1 e 500:"))
